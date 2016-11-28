4063e535c373cf8c705d695c07c0a1b377fd9f6c
Felix Sabatié <felix.sabatie@gmail.com>
2016-11-28 08:49:31 +0100

    Ajout Client + Technician

---
+++ database/migrations/2016_11_28_074247_create_clients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('city');
+            $table->string('postalCode');
+            $table->string('address1');
+            $table->string('address2')->nullable();
+            $table->integer('user_id')->unsigned()->index();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
